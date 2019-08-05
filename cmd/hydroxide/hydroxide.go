019846f5620055af6940019911014246dec5caa8
chebyte <mauro@hashdog.com>
2019-08-05 17:22:31 -0300

    Update paths

--- cmd/hydroxide/hydroxide.go
+++ cmd/hydroxide/hydroxide.go
@@ -207,18 +207,7 @@ func main() {
 		}
 
 		fmt.Println("Bridge password:", bridgePassword)
-		os.Setenv("BRIDGE", bridgePassword)
-		exec.Command("echo " + bridgePassword + " > test.txt")
-		exec.Command("export BRIDGE=" + bridgePassword)
-		exec.Command("touch prueba")
-		out, err := exec.Command("ls").Output()
-
-		// if there is an error with our execution
-		// handle it here
-		if err != nil {
-			fmt.Printf("%s", err)
-		}
-		fmt.Println("Bridge passworddd: " + bridgePassword + "test")
+		exec.Command("bash", "-c", "export", "BRIDGE="+bridgePassword)
 
 	case "status":
 		usernames, err := auth.ListUsernames()
